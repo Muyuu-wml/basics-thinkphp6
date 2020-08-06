39874a0bdd312c90c2665366720fd4d471e2f71a
Muyuu <1013300437@qq.com>
2020-08-06 19:45:11 +0800

    修改模型

--- app/index/model/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\index\model;
-
-use think\Model;
-
-class UserModel extends Model
-{
-    protected $autoWriteTimestamp = true;
-}
---
+++ app/model/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\model;
+
+use think\Model;
+
+class UserModel extends Model
+{
+    protected $autoWriteTimestamp = true;
+}
