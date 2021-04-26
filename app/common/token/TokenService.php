a1170ff01a874887a6c3214e2c86549592759fef
Muyuu <1013300437@qq.com>
2021-04-26 17:56:49 +0800

    'tokenserver'

--- app/common/token/TokenService.php
+++ app/common/token/TokenService.php
@@ -12,20 +12,22 @@ use think\facade\Env;
 
 class TokenService extends BaseController
 {
-    /**
-     * 获取token
-     *
-     * @param [type] $user_id
-     * @return void
+    /**获取token
+     * @param $user_id
+     * @param string $access_jwt_key
+     * @return array
      */
-    public static function getToken($user_id)
+    public static function getToken($user_id, $access_jwt_key = '')
     {
+        if(empty($access_jwt_key)) {
+            $access_jwt_key = config('system.access_jwt_key');
+        }
         if (Env::get('APP.DOUBLE_TOKEN')) {
             $access_token_arr = [
                 'user_id'     => $user_id,
                 'expire_time' => strtotime('+2 hours') // access_token的过期时间为2小时
             ];
-            $access_jwt_token = JWT::encode($access_token_arr, config('system.access_jwt_key'));
+            $access_jwt_token = JWT::encode($access_token_arr, $access_jwt_key);
     
             $refresh_token_arr = [
                 'user_id'     => $user_id,
@@ -42,7 +44,7 @@ class TokenService extends BaseController
                 'user_id'     => $user_id,
                 'expire_time' => strtotime('+10 day') // access_token的过期时间为10天
             ];
-            $access_jwt_token = JWT::encode($access_token_arr, config('system.access_jwt_key'));
+            $access_jwt_token = JWT::encode($access_token_arr, $access_jwt_key);
             $jwt_data = [
                 'access_jwt_token' => $access_jwt_token,
             ];
@@ -58,39 +60,46 @@ class TokenService extends BaseController
      * @param [type] $refresh_token
      * @return void
      */
-    public static function getAccessTokenByRefreshToken($access_token, $refresh_token)
+    public static function getAccessTokenByRefreshToken($access_token, $refresh_token, $access_jwt_key = '')
     {
-        $access_jwt_key = config('system.access_jwt_key');
-        $refresh_jwt_token = config('system.refresh_jwt_token');
+        if (empty($access_jwt_key)) {
+            $access_jwt_key = config('system.access_jwt_key');
+        }
+        $refresh_jwt_key = config('system.refresh_jwt_key');
 
         try {
             $access_token_arr = (array)JWT::decode($access_token, $access_jwt_key, array('HS256'));
-            $refresh_jwt_arr = (array)JWT::decode($refresh_token, $refresh_jwt_token, array('HS256'));
+            $refresh_token_arr = (array)JWT::decode($refresh_token, $refresh_jwt_key, array('HS256'));
         } catch (\UnexpectedValueException $exception) {
-            error('Token Error', [], 403);
+            error('Token Error', [], 402);
         }
 
-        if (isset($access_token_arr['user_id']) && isset($access_token_arr['expire_time']) && isset($refresh_jwt_arr['user_id']) && isset($refresh_jwt_arr['expire_times'])) {
-            if (time() > $access_token_arr['expire_time'] && time() < $refresh_jwt_token['expire_time']) {
-                return self::getToken($refresh_jwt_token['useer_id']);
-            } elseif (time() > $access_token_arr['expire_time'] && time() > $refresh_jwt_token['expire_time']) {
-                error('登录认证过期', [], 403);
-            } elseif (time() < $access_token_arr['expire_time'] && time() < $refresh_jwt_token['expire_time']) {
-                return [$access_token, $refresh_token];
+        if (isset($access_token_arr['user_id']) && isset($access_token_arr['expire_time']) && isset($refresh_token_arr['user_id']) && isset($refresh_token_arr['expire_times'])) {
+            if (time() > $access_token_arr['expire_time'] && time() < $refresh_token_arr['expire_time']) {
+                if ($access_token_arr['user_id'] == $refresh_token_arr['user_id']) {
+                    return self::getToken($refresh_token_arr['useer_id']);
+                } else {
+                    error('Invalid RefreshToken', [], 402);
+                }
+            } elseif (time() > $access_token_arr['expire_time'] && time() > $refresh_token_arr['expire_time']) {
+                error('登录认证过期', [], 402);
+            } elseif (time() < $access_token_arr['expire_time'] && time() < $refresh_token_arr['expire_time']) {
+                return ['access_jwt_token' => $access_token, 'refresh_jwt_token' => $refresh_token];
             }
         }
     }
 
-    /**
-     * 检验token
-     *
-     * @param [type] $token
-     * @return void
+    /**检验token
+     * @param $token
+     * @param string $access_jwt_key
+     * @return array
      */
-    public static function checkToken($token)
+    public static function checkToken($token, $access_jwt_key = '')
     {
         //判断token是否为非法的token
-        $access_jwt_key = config('system.access_jwt_key');
+        if (empty($access_jwt_key)) {
+            $access_jwt_key = config('system.access_jwt_key');
+        }
 
         try {
             $token_arr = (array)JWT::decode($token, $access_jwt_key, array('HS256'));
@@ -106,15 +115,15 @@ class TokenService extends BaseController
                 if (!empty($token_arr['user_id']) && !empty($token_arr['expire_time'])) {
                     //判断时间是否过期
                     if (time() <= $token_arr['expire_time']) {
-                        return ['state' => false, 'msg' => 'AccessToken Success', 'user_id' => $token_arr['user_id']];
+                        return ['state' => false, 'msg' => 'Valid AccessToken', 'user_id' => $token_arr['user_id']];
                     } else {
                         return ['state' => false, 'msg'=> '登录过期请重新登录','user_id' => $token_arr['user_id']];
                     }
                 } else {
-                    return ['state' => false, 'msg'=> 'AccessToken Error','user_id' => ''];
+                    return ['state' => false, 'msg'=> 'Invalid AccessToken'];
                 }
             } else {
-                return ['state' => false, 'msg'=> 'AccessToken Error','user_id' => ''];
+                return ['state' => false, 'msg'=> 'Invalid AccessToken'];
             }
         }
     }
